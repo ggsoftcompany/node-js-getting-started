e7bf23f47cbbb472779d8218259ac806046d64d0
ggsoftcompany <58540644+ggsoftcompany@users.noreply.github.com>
2022-08-23 13:26:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const UI_URL = process.env.UI_URL || "https://default.ui-url.com"
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('pages/index', {data: {url: UI_URL}}))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
