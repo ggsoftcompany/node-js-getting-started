99fa2bf7d94c18138bd138e2b259ac921587ce1e
ggsoftcompany <58540644+ggsoftcompany@users.noreply.github.com>
2022-09-13 14:21:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express')
+const redis = require('redis')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const UI_URL = process.env.UI_URL || "https://default.ui-url.com"
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index', {data: {url: UI_URL}}))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+async function init(){
+    const redisClient = redis.createClient({
+        url: 'rediss://:GRIkw3TjFIrAKlqwSBntPOENpMaBUtyF3AzCaABKcpE=@ryder-whiplash.redis.cache.windows.net:6380',
+    });
+
+    await redisClient.connect();
+    redisClient.on('connect', function() {
+        console.log('Redis client connected');
+    });
+    redisClient.on('error',
+        function (err) {
+            console.log('Something went wrong ' + err);
+        });
+
+    express()
+        .use(express.static(path.join(__dirname, 'public')))
+        .set('views', path.join(__dirname, 'views'))
+        .set('view engine', 'ejs')
+        .get('/', async (req, res) => {
+            //  await redisClient.set('key1', 'pepito');
+            const value = await redisClient.get('key1');
+            res.render('pages/index', {data: {name: value}});
+        })
+        .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+}
+
+init().then(r => {});
